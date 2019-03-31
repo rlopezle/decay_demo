ad82978269689916c308a8f7b74bd9ff6228859b
rlopez <rlopezle@umich.edu>
2019-03-31 19:16:18 -0400

    updating files and readme

--- README.md
+++ README.md
@@ -10,13 +10,33 @@ The goal for this exercise is to gain experience with the basics of the Fortran 
 
 The radioactive decay chain problem that will be worked out is as follows:
 
-One is given that a parent nuclide has a half life of 5 days and a daughter nuclide has one of 7 days. The granddaughter is stable and the initial population of the parent nuclide is 1E6 while the populations of the daughter and granddaughter are 0. Show the behaviour of the three nuclide populations by plotting all three on top of one another.
+One is given that a parent nuclide has a half life of 5 days and a daughter nuclide has one of 7 days. The granddaughter is stable and the initial population of the parent nuclide is 1E6 while the populations of the daughter and granddaughter are 0. Show the behavior of the three nuclide populations by plotting all three on top of one another.
 
+## Problem Setup
+
+Before we begin coding the problem, we'll need to determine what expressions we will use in the code to obtain the populations at any time *t*. To do so we start witht the following differential equations that represent the scenario of the problem:
+
+![Differential equations](images/diff_eq.png)
+
+Solving the differential equations allows us to obtain the final expressions for the parent population [N1(t)], daughter population [N2(t)], and granddaughter population [N3(t)]:
+
+![Final expressions](images/solutions.png)
 
 
 ## Coding in Python
 
 The methods by which this problem was initially solved using Python can be seen in the jupyter notebook that contains the [Python portion of the exercise.](python_demo.ipynb) The final result is the following plot:
 ![Python plot of populations](images/pyplot.png)
+
+## Coding in Fortran
+The Fortran file that contains the code may be found [here along with comments.](decay_chain.f) The output of the Fortran program is a .CSV file with the population of the the three nuclides in 1 hour increments. The raw file looks like so:
+![Raw .CSV file](images/raw_csv.png)
+
+Using the raw data one can obtain a plot to compare with the one obtained from Python:
+![Fortran plot of populations](images/fortran_final.png)
+
+The final plot obtained from the Fortran data was in accordance with the expected behavior. Thus we conlcude that the exercise was a success as the desired reults were obtained using some basic Fortran coding!
+
+
 
 
--- decay_chain.f
+++ decay_chain.f
@@ -1,8 +1,10 @@
 PROGRAM decay_chain
 
+! The following line below may be used to compile this program:
+! gfortran -o decay_chain decay_chain.f -ffree-form
+
 implicit none
 
-! gfortran -o decay_chain decay_chain.f -ffree-form
 integer, parameter :: max_size = 2000
 integer, parameter :: output = 7
 integer :: i
@@ -15,46 +17,55 @@ real :: N_2_pop(max_size)
 real :: N_3_pop(max_size)
 real, dimension(max_size,4) :: results
 
+!Prompts the user for the half lives of parent and daughter and then reads them in
 write (*,*) 'Enter half lives of parent and daughter nuclei (in days): '
 read (*,*) hl_1, hl_2
 
+!Prints back out user specified info for reference
 write (*,*) 'The half lives are: ', hl_1, ' days and ', hl_2, ' days'
 
+!Initializes initial population of parent nuclide
 N_0 = 1E6
+!Calulates the lambdas of the parent and daughter in inverse seconds
 lambda_1 = log(2.)/(hl_1*24.*60.*60.)
 lambda_2 = log(2.)/(hl_2*24.*60.*60.)
 
-!here
+!Creates time array of 3600 second (1 hour) increments
 do i = 1, max_size
 	time(i) = (i-1)*3600.
 end do
-
+!Creates array of parent population using expression from README.md file
 do i = 1, max_size
 	N_1_pop(i) = N_0 * exp(-1.*lambda_1*time(i))
 end do
-
+!Creates array of daughter population using expression from README.md file
 do i = 1, max_size
 	N_2_pop(i) = ((lambda_1*N_0)/(lambda_2 - lambda_1)) * (exp(-1.*lambda_1*time(i)) - exp(-1.*lambda_2*time(i)))
 end do
-
+!Creates array of granddaughter population using expression from README.md file
 do i = 1, max_size
 	N_3_pop(i) = (N_0/(lambda_2 - lambda_1)) * ((lambda_2*(1. - exp(-1.*lambda_1*time(i)))) &
 	-(lambda_1 *(1. - exp(-1.*lambda_2*time(i)))))
 end do
 
+!Fills each column of the results array with time and populations
 results(:,1) = time(:)
 results(:,2) = N_1_pop(:)
 results(:,3) = N_2_pop(:)
 results(:,4) = N_3_pop(:)
 
+!Opening a CSV file for preparation of outputting the numbers from results array
 open(unit=output,file='OUTPUT.csv')
+!Writes out the first line of the CSV file which will just be the lables for the columns
 write(output, '(a12,",",a17,",",a19,",",a24)') 'Time_(hours)', 'Parent_Population', 'Daughter_Population', &
      'Granddaughter_Population'
-
+!Iterates through the results array and writes out each row
 do i = 1, max_size
     write(output, '(f0.0,",",f0.4,",",f0.4,",",f0.4)') results(i,1)/3600., results(i,2), results(i,3), results(i,4)
 end do
 
+!Closes the CSV file
 close (output)
 
+!Ends the program
 END PROGRAM decay_chain
